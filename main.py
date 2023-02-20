977db2c732133b10a4f92b74a5802501a038f5bd
Lassi Hautala <lassi.j.hautala@utu.fi>
2023-02-21 00:56:55 +0200

    Dictiä voi nyt järjestellä. Jos seuraavan päivän hintoja ei ole julkaistu, ohjelma kaatuu.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 import xmltodict
 from datetime import datetime, timedelta
+from operator import itemgetter
 
 def getSPOT(today, tomorrow):
 
     payload = {'securityToken':'3baa6934-0db0-4356-8687-c4ab205b6305','documentType':'A44','in_Domain':'10YFI-1--------U', 'out_Domain':'10YFI-1--------U',
     'periodStart': f'{today}0000','periodEnd':f'{tomorrow}0000'}
-
 
     r = requests.get('https://web-api.tp.entsoe.eu/api', params=payload)
 
@@ -22,9 +22,14 @@ tomorrow = tomorrow.strftime('%Y%m%d')
 print(today)
 print(tomorrow)
 
+today = '20230218'
+tomorrow = '20230219'
+
 spot = getSPOT(today, tomorrow)
 
 spotToday = spot["Publication_MarketDocument"]["TimeSeries"][0]["Period"]["Point"]
 spotTomorrow = spot["Publication_MarketDocument"]["TimeSeries"][1]["Period"]["Point"]
-print(spotToday)
-print('\n', spotTomorrow)
+
+sort = sorted(spotToday, key=lambda x: float(x['price.amount']), reverse=False)
+
+print(sort)
