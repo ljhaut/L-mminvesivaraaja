37550f22831536c2c26a8d4ddda7c4921d32da04
Lassi Hautala <lassi.j.hautala@utu.fi>
2023-02-20 23:47:05 +0200

    Datan fetchaus onnistuu ja sen parsiminen toimii oikein

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import requests
 import json
+import re
 import xmltodict
+import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
 
-def getSPOT():
+def getSPOT(today, tomorrow):
 
-    payload = {'securityToken':'3baa6934-0db0-4356-8687-c4ab205b6305','documentType':'A44',
-    'periodStart':'202210280000','periodEnd':'202210290000'}
+    payload = {'securityToken':'3baa6934-0db0-4356-8687-c4ab205b6305','documentType':'A44','in_Domain':'10YFI-1--------U', 'out_Domain':'10YFI-1--------U',
+    'periodStart': f'{today}0000','periodEnd':f'{tomorrow}0000'}
+
 
     r = requests.get('https://web-api.tp.entsoe.eu/api', params=payload)
 
-    print (r.content)
+    xml_string = r.content.decode('utf-8')
 
-    with open(r.content) as xmlfile:
-        data = xmltodict.parse(xmlfile.read())
-        jsondata = json.dumps(data)
+    xml_dict = xmltodict.parse(xml_string)
 
+    return xml_dict
 
-    return jsondata
+today = datetime.now().strftime('%Y%m%d')
+tomorrow = datetime.now() + timedelta(1)
+tomorrow = tomorrow.strftime('%Y%m%d')
+print(today)
+print(tomorrow)
 
-getSPOT()
+spot = getSPOT(today, tomorrow)
+
+spotToday = spot["Publication_MarketDocument"]["TimeSeries"][0]["Period"]["Point"]
+spotTomorrow = spot["Publication_MarketDocument"]["TimeSeries"][1]["Period"]["Point"]
+print(spotToday)
+print('\n', spotTomorrow)
